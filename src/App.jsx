88619fe91f599dbd8f440cc63263629a238c3a7e
Kingerjay <ogilikingsley96@gmail.com>
2026-08-01 12:42:11 +0100

    pigeon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './index.css'
 import { Navigate, Route, Routes } from 'react-router-dom'
 // import Home from './pages/Home'
-// import Houstonrealestate from './pages/Houstonrealestate'
 import ColumbusRealEstate from './pages/ColumbusRealEstate'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -10,8 +9,7 @@ import OrderHistory from './components/columbus/OrderHistory'
 import Home from './pages/Home'
 import LasVegasPhotography from './pages/LasVegasPhotography'
 import NyPhotography from './pages/NyPhotography'
-import AnnaMariaIslandVacationHomes from './pages/AnnaMariaIslandVacationHomes'
-import GalvestonIslandVacation from './pages/GalvestonIslandVacation'
+import PigeonForgeRealtor from './pages/PigeonForgeRealtor'
 
 
 function App() {
@@ -20,14 +18,12 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Navigate to="/sale/domain-name=galvestonislandvacation.com" replace />} />
+        <Route path='/' element={<Navigate to="/sale/domain-name=pigeonforgerealtor.com" replace />} />
         
-        {/* <Route path='forsale/houstonrealestate.com' element={<Houstonrealestate />} /> */}
         <Route path='sale/domain-name=columbusrealestate.com' element={<ColumbusRealEstate />} />
         <Route path='sale/domain-name=vegasphotographer.com' element={<LasVegasPhotography />} />
         <Route path='sale/domain-name=nyphotography.com' element={<NyPhotography />} />
-        <Route path='sale/domain-name=annamariaislandvacationhomes.com' element={<AnnaMariaIslandVacationHomes />} />
-        <Route path='sale/domain-name=galvestonislandvacation.com' element={<GalvestonIslandVacation />} />
+        <Route path='sale/domain-name=pigeonforgerealtor.com' element={<PigeonForgeRealtor />} />
         <Route path='checkout' element={<Checkout />} />
         <Route path='order-history' element={<OrderHistory/>} />
         <Route path='home' element={<Home />} />
---
+++ src/pages/PigeonForgeRealtor.jsx
@@ -0,0 +1,17 @@
+import 'react'
+import HomeSection from '../components/HomeSection'
+
+
+function PigeonForgeRealtor() {
+       
+  return (
+    <HomeSection 
+            domainName="PigeonForgeRealtor.com"
+            price={100}
+            checkoutUrl="/checkout"
+        />
+  )
+}
+
+export default PigeonForgeRealtor
+
